DPDK/dpdk

kni: remove needless casts
This fixes cases in KNI where kernel allocation function return value
is needlessly casted.

Detected with coccinelle:
lib/librte_eal/linuxapp/kni/ethtool/igb/igb_main.c:3181:25-28:
	WARNING: casting value returned by memory allocation function to (u32 *) is useless.
lib/librte_eal/linuxapp/kni/kni_vhost.c:690:9-28:
	WARNING: casting value returned by memory allocation function to (struct rte_kni_fifo *) is useless.
lib/librte_eal/linuxapp/kni/kni_vhost.c:684:13-27:
	WARNING: casting value returned by memory allocation function to (struct sk_buff *) is useless

Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

lib/librte_eal/linuxapp/kni/kni_vhost.c
@@ -681,14 +681,12 @@ kni_vhost_backend_init(struct kni_dev *kni)
 	}
 
 	/* cache init */
-	q->cache = (struct sk_buff*)
-		kzalloc(RTE_KNI_VHOST_MAX_CACHE_SIZE * sizeof(struct sk_buff),
-			GFP_KERNEL);
+	q->cache = kzalloc(RTE_KNI_VHOST_MAX_CACHE_SIZE * sizeof(struct sk_buff),
+			   GFP_KERNEL);
 	if (!q->cache)
 		goto free_fd;
 
-	fifo = (struct rte_kni_fifo*)
-		kzalloc(RTE_KNI_VHOST_MAX_CACHE_SIZE * sizeof(void *)
+	fifo = kzalloc(RTE_KNI_VHOST_MAX_CACHE_SIZE * sizeof(void *)
 			+ sizeof(struct rte_kni_fifo), GFP_KERNEL);
 	if (!fifo)
 		goto free_cache;

miguelinux/dpdk-modules

    WARNING: casting value returned by memory allocation function to (u32 *) is useless.
    WARNING: casting value returned by memory allocation function to (struct rte_kni_fifo *) is useless.
    WARNING: casting value returned by memory allocation function to (struct sk_buff *) is useless
Signed-off-by: Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>

drivers/net/dpdk/kni/kni_vhost.c
@@ -645,14 +645,12 @@ kni_vhost_backend_init(struct kni_dev *kni)
 	}
 
 	/* cache init */
-	q->cache = (struct sk_buff*)
-		kzalloc(CONFIG_DPDK_KNI_VHOST_MAX_CACHE_SIZE * sizeof(struct sk_buff),
+	q->cache = kzalloc(CONFIG_DPDK_KNI_VHOST_MAX_CACHE_SIZE * sizeof(struct sk_buff),
 			GFP_KERNEL);
 	if (!q->cache)
 		goto free_fd;
 
-	fifo = (struct rte_kni_fifo*)
-		kzalloc(CONFIG_DPDK_KNI_VHOST_MAX_CACHE_SIZE * sizeof(void *)
+	fifo = kzalloc(CONFIG_DPDK_KNI_VHOST_MAX_CACHE_SIZE * sizeof(void *)
 			+ sizeof(struct rte_kni_fifo), GFP_KERNEL);
 	if (!fifo)
 		goto free_cache;