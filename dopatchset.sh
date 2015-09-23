eccea82be0a1d8d2219224e1028b3ef9b53b8ac1
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2015-09-23 16:49:09 -0500

    Add dopatchset
    This scripts helps to create a DPDK patchset
    
    Signed-off-by: Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>

---
+++ dopatchset.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+LVER=linux-4.2.y
+REPO=$(mktemp -d)
+
+git -C ${REPO} init
+mkdir ${REPO}/drivers/net
+
+# Get Kconfig & Makefile from kernel
+git checkout ${LVER} drivers/net/Kconfig drivers/net/Makefile
+# Copy them to new repo
+cp drivers/net/Kconfig drivers/net/Makefile ${REPO}/drivers/net
+# Restore them
+git checkout drivers/net/Kconfig drivers/net/Makefile
+
+git -C ${REPO} add --all
+git -C ${REPO} commit -s -m "${LVER} Makefile & Kconfig"
+
+# Copy dpdk sources
+cp -r  drivers/net/dpdk ${REPO}/drivers/net
+git -C ${REPO} add --all
+git -C ${REPO} commit -s -m "Add DPDK source files"
+
+cp drivers/net/Kconfig drivers/net/Makefile ${REPO}/drivers/net
+git -C ${REPO} add --all
+git -C ${REPO} commit -s -m "Integrate Kconfig and Makefiles"
+
+git -C ${REPO} format-pach --start-number 5001 -2
+
