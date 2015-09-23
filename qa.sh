5e488c6df78e5719f5bbd86adb7a21d4ce2b1983
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2015-09-23 14:47:02 -0500

    add QA.sh script
    This script will help to assure the sync between
    DPDK upstream and this repo
    
    Signed-off-by: Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>

---
+++ qa.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+DPDK=$1
+DPDK_EAL=${DPDK}/lib/librte_eal
+#Kernel DPDK
+KD=drivers/net/dpdk
+
+# Delete previoes Q/A branch
+git branch -D qa
+
+# Create new branch to do QA
+git checkout -b qa master
+
+# Pull the lastes DPDK repo
+git -C ${DPDK} pull
+
+
+for f in rte_pci_dev_feature_defs.h  rte_pci_dev_features.h
+do
+	cp ${DPDK_EAL}/common/include/${f} ${KD}
+done
+
+cp ${DPDK_EAL}/linuxapp/igb_uio/igb_uio.c ${KD}
+
+cp ${DPDK_EAL}/linuxapp/kni/*.c ${KD}/kni
+cp ${DPDK_EAL}/linuxapp/kni/*.h ${KD}/kni
+
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/igb/*.h ${KD}/kni
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/igb/*.c ${KD}/kni
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/igb/kcompat.h ${KD}/kni/kcompact_igb.h
+
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/ixgbe/*.h ${KD}/kni
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/ixgbe/*.c ${KD}/kni
+
+git add -A
+git commit -m "QA copy"
+
+# Change to master
+git checkout master
+
