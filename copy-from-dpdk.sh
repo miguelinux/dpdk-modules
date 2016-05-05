27bc96ca27f1dee21466adbca3fb5d3878b27efc
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2016-05-05 17:56:49 -0500

    copy files from dpdk

--- copy-from-dpdk.sh
+++ copy-from-dpdk.sh
@@ -15,6 +15,8 @@ DPDK_EAL=${DPDK}/lib/librte_eal
 KD=drivers/net/dpdk
 
 mkdir -p ${KD}
+mkdir -p ${KD}/kni
+mkdir -p ${KD}/kni/exec-env
 
 for f in rte_pci_dev_feature_defs.h  rte_pci_dev_features.h
 do
@@ -24,3 +26,35 @@ done
 cp ${DPDK_EAL}/linuxapp/igb_uio/igb_uio.c ${KD}
 cp ${DPDK_EAL}/linuxapp/igb_uio/compat.h ${KD}
 
+cp ${DPDK_EAL}/common/include/rte_pci_dev_ids.h ${KD}/kni
+cp ${DPDK_EAL}/linuxapp/eal/include/exec-env/rte_kni_common.h ${KD}/kni/exec-env
+
+cp ${DPDK_EAL}/linuxapp/kni/*.c ${KD}/kni
+cp ${DPDK_EAL}/linuxapp/kni/*.h ${KD}/kni
+
+#
+for f in ${KD}/kni/*.h ${KD}/kni/*.c
+do
+    sed -i "s/RTE_KNI_PREEMPT_DEFAULT/CONFIG_DPDK_KNI_PREEMPT_DEFAULT/g" $f;
+    sed -i "s/RTE_KNI_KO_DEBUG/CONFIG_DPDK_KNI_KO_DEBUG/g" $f;
+    sed -i "s/RTE_KNI_VHOST/CONFIG_DPDK_KNI_VHOST/g" $f;
+#    sed -i "s/RTE_KNI_VHOST_VNET_HDR_EN/CONFIG_DPDK_KNI_VHOST_VNET_HDR_EN/g" $f;
+#    sed -i "s/RTE_KNI_VHOST_DEBUG_RX/CONFIG_DPDK_KNI_VHOST_DEBUG_RX/g" $f;
+#    sed -i "s/RTE_KNI_VHOST_DEBUG_TX/CONFIG_DPDK_KNI_VHOST_DEBUG_TX/g" $f;
+done
+
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/igb/*.h ${KD}/kni
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/igb/kcompat.h ${KD}/kni/kcompat_igb.h
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/igb/*.c ${KD}/kni
+
+for f in ${KD}/kni/igb.h ${KD}/kni/e1000_osdep.h
+do
+    sed -i "s/kcompat.h/kcompat_igb.h/g" $f;
+done
+
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/ixgbe/*.h ${KD}/kni
+cp ${DPDK_EAL}/linuxapp/kni/ethtool/ixgbe/*.c ${KD}/kni
+
+# Remove unneeded file
+rm ${KD}/kni/igb_ptp.c
+rm ${KD}/kni/kcompat.c
